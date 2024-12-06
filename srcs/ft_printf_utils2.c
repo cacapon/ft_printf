7b5b398a9775716b2d95ed3a3a6d7e95673c7f41
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2024-12-06 12:16:52 +0900

    0x0 -> (nil)に変更

--- srcs/ft_printf_utils2.c
+++ srcs/ft_printf_utils2.c
@@ -6,7 +6,7 @@
 /*   By: ttsubo <ttsubo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 15:09:58 by ttsubo            #+#    #+#             */
-/*   Updated: 2024/12/06 11:41:33 by ttsubo           ###   ########.fr       */
+/*   Updated: 2024/12/06 12:13:39 by ttsubo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ size_t	ft_putptr_fd(void *ptr, int fd)
 	size_t	len;
 
 	if (!ptr)
-		return (ft_putstr_fd("0x0", fd));
+		return (ft_putstr_fd("(nil)", fd));
 	len = ft_putstr_fd("0x", fd);
 	len += _ft_putptr_fd_rec((uintptr_t)ptr, fd);
 	return (len);
